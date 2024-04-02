478a9945668cbcaca1b563d177020e694a7686ef
Zennerharauf <163380432+Zennerharauf@users.noreply.github.com>
2024-04-03 00:13:20 +0500

    Add files via upload

---
+++ 10.program_1.ts
@@ -0,0 +1,8 @@
+
+
+//program written by: zennerha
+//current date: 28/03/24
+
+//this program wiil write HELLO WORLD
+
+console.log("Hello World");
---
+++ 10.program_2.ts
@@ -0,0 +1,8 @@
+
+
+//program written by: zennerha
+//current date: 28/03/24
+
+//this program wiil add two digits.
+
+console.log(2+2);
