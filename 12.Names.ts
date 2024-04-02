2843a2b8ae9c6df133f42b92bbbe20628c2e3cf9
Zennerharauf <163380432+Zennerharauf@users.noreply.github.com>
2024-04-03 00:15:47 +0500

    Add files via upload

---
+++ 12.Names.ts
@@ -0,0 +1,12 @@
+
+
+let names : string[] = [ "AbdulRauf","Shazia","Jawad","Zennerha","Sadiq"] ;
+
+
+const message : string = 'welcome to Typescript class:'
+
+for(let i=0; i<names.length; i++){
+
+console.log(message + names[i]) ;
+
+}
